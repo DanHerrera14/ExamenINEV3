6d80d739ae38bec98e2cc419e927e9dc1dc71d66
DanHerrera14 <daniel.avispl@gmail.com>
2023-04-29 20:55:22 -0500

    se agrega el proyecto

---
+++ Script.js
@@ -0,0 +1,64 @@
+const arrayINE={
+    apPaterno:"HERRERA",
+    apMaterno: "CHI",
+    nombres: "EDGAR DANIEL",
+    sexo: "SEXO H",
+    domicilio:"SUPMZA 96 77535,BENITO JUAREZ Q.ROO",
+    claveelector: "Clave de elector HRCHED91121523H700",
+    curp: "HECE911215HQRRHD08",
+    registro: "2008 01",
+    vigencia: "2021-2031",
+    fechanacimiento: "15/12/1991",
+    seccion: "0151",
+    escudo:"https://cdn.pixabay.com/photo/2018/08/18/18/38/coat-of-arms-3615253_1280.png",
+    foto:"https://static.vecteezy.com/system/resources/previews/015/119/100/non_2x/businessman-icon-man-icon-design-illustration-free-png.png",
+    mapa:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Mexico_Map.svg/1200px-Mexico_Map.svg.png",
+    mapacentro:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Mexico_Map.svg/1200px-Mexico_Map.svg.png",
+    piramide:"https://st3.depositphotos.com/22569638/36828/v/450/depositphotos_368284128-stock-illustration-detailed-and-naturalistic-vector-graphic.jpg",
+    firma:"https://e7.pngegg.com/pngimages/664/282/png-clipart-autogram-ishimbayev-law-firm-signature-others-angle-monochrome.png",
+    maguey:"https://image.pngaaa.com/917/1766917-small.png",
+    greco:"Imagen/greco.png"
+
+} ;
+
+
+let apPaterno=arrayINE.apPaterno;
+let apMaterno=arrayINE.apMaterno;
+let nombres=arrayINE.nombres;
+let sexo=arrayINE.sexo;
+let domicilio=arrayINE.domicilio;
+let claveelector=arrayINE.claveelector;
+const curp = arrayINE.curp;
+let registro= arrayINE.registro;
+let fechanacimiento = arrayINE.fechanacimiento;
+let seccion=arrayINE.seccion;
+let vigencia=arrayINE.vigencia;
+let escudo=arrayINE.escudo;
+let foto=arrayINE.foto;
+let mapa=arrayINE.mapa;
+let mapacentro=arrayINE.mapacentro;
+let piramide=arrayINE.piramide;
+let firma=arrayINE.firma;
+let maguey=arrayINE.maguey;
+let greco=arrayINE.greco;
+
+document.getElementById("apPaterno").innerHTML=apPaterno
+document.getElementById("apMaterno").innerHTML=apMaterno
+document.getElementById("nombres").innerHTML=nombres
+document.getElementById("domicilio").innerHTML=domicilio
+document.getElementById("claveelector").innerHTML=claveelector
+document.getElementById("fechanacimiento").innerHTML=fechanacimiento
+document.getElementById("sexo").innerHTML=sexo
+document.getElementById("registro").innerHTML=registro
+document.getElementById("curp").innerHTML=curp
+document.getElementById("vigencia").innerHTML=vigencia
+document.getElementById("seccion").innerHTML=seccion
+document.getElementById("escudo").src=escudo
+document.getElementById("foto").src=foto
+document.getElementById("foto2").src=foto
+document.getElementById("mapa").src=mapa
+document.getElementById("mapacentro").src=mapacentro
+document.getElementById("piramide").src=piramide
+document.getElementById("firma").src=firma
+document.getElementById("maguey").src=maguey
+document.getElementById("greco").src=greco
